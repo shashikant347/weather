c9c4cda8551c275715af836c297dfe17103a0439
shashikant347 <yshashikant231217@gmail.com>
2025-07-15 20:39:18 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
-const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?q="
 const apikey = "4bdceced7bd48b6ec3404f1ccbde833c" ;
 
 
+
+let input = document.querySelector('#input')
+let searchbutton = document.querySelector('#searchBTN')
 
 
 
@@ -10,20 +14,20 @@ async function checkWeather(city) {
   const response = await fetch(apiurl + city + `&appid=${apikey}`)
   var data = await response.json();
   
-  console.log(data)
+  console.log(data.wind.speed)
+  
+  
+  document.querySelector('.city-name').innerHTML = data.name;
+  document.querySelector('.temp').innerHTML = Math.round(((data.main.temp)-273.15)) +'°C';
+  document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+  document.querySelector('#wind').innerHTML = data.wind.speed + "km/h";
+
+  
   
 }
 
 
 
-let input = document.querySelector('#input')
-
-let searchbutton = document.querySelector('#searchBTN')
-
-document.querySelector('.city-name').innerHTML= data.name
-document.querySelector('.temp').innerHTML = Math.round(data.main.temp) +'°C';
-document.querySelector('.humidity').innerHTML= main.humidity + '%';
-document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h'
 
 searchbutton.addEventListener('click',()=>{
   checkWeather(input.value);
